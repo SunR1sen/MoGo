535b9d813e511b54ed593e4f404f3c9035e1b926
SunR1sen <sunr1sen.dev@gmail.com>
2020-05-21 18:58:14 +0300

    Finished layout (v 1.0)
    Fully adaptive, maked interactive menu
    and slider for reviews (slick-slider).

--- js/app.js
+++ js/app.js
@@ -26,6 +26,68 @@ $(function() {
 
 
     // Smooth scroll
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
 
+        var $this = $(this),
+            blockId = $this.data('scroll'),
+            blockOffset = $(blockId).offset().top;
+
+        $("#nav a").removeClass("active");
+        $this.addClass("active");
+
+
+        $("html, body").animate({
+            scrollTop: blockOffset
+        }, 500);
+    });
+
+
+    // Logos 
+    $("#logo").on("click", function(event) {
+        // event.preventDefault();
+
+        $("#nav").removeClass("active");
+        $("#nav_toggle").removeClass("active");
+        
+    });
+
+    // Menu nav toggle 
+    $("#nav_toggle").on("click", function(event) {
+        event.preventDefault();
+
+        $(this).toggleClass("active");
+        $("#nav").toggleClass("active");
+
+    });
+
+    // Close nav 
+    $("#nav a").on("click", function(event) {
+        event.preventDefault();
+
+        $("#nav").removeClass("active");
+        $("#nav_toggle").removeClass("active");
+    });
+
+
+    // Collpse
+    $("[data-collapse]").on("click", function(event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $this.data('collapse');
+
+        $this.toggleClass("active");
+    });
+
+
+    // Slider
+    $("[data-slider]").slick({
+        infinite: true,
+        fade: false,
+        slideToShow: 1,
+        slideToScroll: 1,
+
+    });
 
 });
